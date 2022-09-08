0de6ef3ebf989197f36abd8baaa598281b2c1d8c
Kaeldehta <floxx33@gmail.com>
2022-09-08 10:58:39 +0200

    feat: add dummy success function

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const SemanticReleaseError = require("@semantic-release/error");
+const process = require('process');
+
+const SECRET = process.env.STUDIP_SECRET;
 
 async function success(pluginConfig, context) {
 
   const {url, prerelease} = pluginConfig;
 
+  console.log(context);
+  
 }
 
 async function verifyConditions(pluginConfig, context) {
@@ -12,6 +17,14 @@ async function verifyConditions(pluginConfig, context) {
 
   if(!url) {
     throw new SemanticReleaseError('Missing parameter url in plugin configuration', 'EINVALIDCONFIG');
+  }
+
+  if(!SECRET) {
+    throw new SemanticReleaseError('Missing environment variable STUDIP_SECRET', 'EINVALIDCONFIG');
+  }
+
+  if(prerelease && typeof prerelease !== 'boolean') {
+    throw new SemanticReleaseError('Invalid value for parameter prerelease in plugin configuration', 'EINVALIDCONFIG');
   }
 
 }
